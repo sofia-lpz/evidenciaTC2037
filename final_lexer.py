0f30d9b2f98bd1a5c053fcbfb763da54d76ed308
sofia-lpz <a01028251@tec.mx>
2024-05-26 21:14:14 -0600

    wip

---
+++ final_lexer.py
@@ -0,0 +1,12 @@
+
+
+
+digito="0123456789"
+letra="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+underscore="_"
+reservadas = [":","if","else","elif","for","while","with","as","def","in","class",
+              "import", "range","print","return","break","try","catch","finally","input","len"]
+
+tabla = [
+
+]
